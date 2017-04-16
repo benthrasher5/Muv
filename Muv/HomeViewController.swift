bd66751b5a1fd7627b8d668884fed22689e78325
Ben Thrasher <benthrasher5@gmail.com>
2017-04-16 12:15:07 -0500

    lots added, but basic skeleton in place

--- Muv/HomeViewController.swift
+++ Muv/HomeViewController.swift
@@ -15,6 +15,14 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        logoImageView.contentMode = .scaleAspectFit
+        
+        let logoImage = UIImage(named: "logoPlain44")
+        logoImageView.image = logoImage
+        
+        navigationItem.titleView = logoImageView
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Muv/SettingsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.swift
+//  Muv
+//
+//  Created by Ben Thrasher on 4/11/17.
+//  Copyright © 2017 Ben Thrasher. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func goHome(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
